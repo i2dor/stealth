2495edce2707a4187bc8003b0af6df6e55e42bba
i2dor <hotelswelt@gmail.com>
2026-03-16 16:02:55 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080'
+      '/api': 'https://stealth-backend-lv38.onrender.com'
     }
   }
 })
